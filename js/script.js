2856bbe0bd879bd3b2dce5187258ab594f495034
Alex Cash <alexcash@me.com>
2012-04-05 00:13:38 -0500

    changed jquery class for twitter so 404 shows as 404

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
 jQuery(function($){
     jQuery(function($){
-        $(".tweet").tweet({
+        $(".tweetr").tweet({
             username: "alexcash",
             join_text: "auto",
             avatar_size: null,
