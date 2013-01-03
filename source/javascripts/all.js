637f6d480e4ab6f2854ab8e7950d4381de643b4f
alexcash <alexcash@me.com>
2013-01-02 22:11:59 -0800

    simplify layout and design. add tweets.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,16 @@
 //= require_tree .
+
+jQuery(function($){
+    $(".tweet").tweet({
+        username: "alexcash",
+        join_text: "auto",
+        avatar_size: null,
+        count: 10,
+        auto_join_text_default: "",
+        auto_join_text_ed: "",
+        auto_join_text_ing: "",
+        auto_join_text_reply: "",
+        auto_join_text_url: "",
+        loading_text: "loading tweets..."
+    });
+});
