97d601bf3385b9c20cc4806b799e0f5ebb07b785
alexcash <alexcash@me.com>
2014-03-15 23:17:29 -0700

    lol @ twitter

---
+++ source/blog/they-pull-me-back-in.markdown
@@ -0,0 +1,9 @@
+---
+title: "Just when I thought I was out, they pull me back in."
+date: 2014/04/15
+description: ""
+published: false
+priority: 0.8
+---
+
+yup
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,10 +1,16 @@
 //= require jquery-2.1.0.js
+//= require lodash.js
+//= require moment.min.js
 //= require jquery.last.fm.js
 
 
 //doc ready
 $(function($){
     'use strict';
+
+    var _ = window._,
+        moment = window.moment;
+
     //populate top albums
     $('.albums').lfm({
         APIkey: '4583963e1d8402859dd8f6e3893625fe',
@@ -13,9 +19,57 @@ $(function($){
         Behavior: 'hover'
     });
 
-    setTimeout(function () {
-        $('.tweets').append($('.stream ol', $('iframe').first().contents()));
-    }, 1000);
+    $.ajax({
+        url:'https://spreadsheets.google.com/feeds/list/0AnMG6z98bgj6dGJCTlJMc3FvZVdiY3FOTjVteklKQWc/od6/public/values?alt=json',
+        type:'get'
+    }).then(function(data){
+        var tweetObjects = data.feed.entry,
+            tweets = [],
+            outTweets = [];
 
+        _.each(tweetObjects, function(tweetObject){
+            tweets.push(_.pick(tweetObject, function(value, key){
+                return key.indexOf('gsx') > -1;
+            }));
+        });
+
+        _.each(tweets, function(tweet){
+            var vals = [];
+            _.forIn(tweet, function(value){
+                vals.push(value['$t']);
+            });
+            outTweets.unshift({
+                handle: vals[1],
+                body: linkify(_.escape(vals[2])),
+                url: vals[3],
+                timestamp: moment(vals[0], 'MMM DD, YYYY at hh:mma').fromNow()
+            });
+        });
+
+        $('.tweets').append(_.template($('#tweets-template').html(), {
+            tweets: _.first(outTweets, 10)
+        }, {
+            evaluate:    /\{\{#([\s\S]+?)\}\}/g,            // {{# console.log("blah") }}
+            interpolate: /\{\{[^#\{]([\s\S]+?)[^\}]\}\}/g,  // {{ title }}
+            escape:      /\{\{\{([\s\S]+?)\}\}\}/g,         // {{{ title }}}
+        }));
+    });
 
 });
+
+
+function linkify(text) {
+    text = text.replace(/(https?:\/\/\S+)/gi, function (s) {
+        return '<a href="' + s + '">' + s + '</a>';
+    });
+
+    text = text.replace(/(^|)@(\w+)/gi, function (s) {
+        return '<a href="http://twitter.com/' + s + '">' + s + '</a>';
+    });
+
+    text = text.replace(/(^|)#(\w+)/gi, function (s) {
+        return '<a href="http://search.twitter.com/search?q=' + s.replace(/#/,'%23') + '">' + s + '</a>';
+     });
+    return text;
+}
+
